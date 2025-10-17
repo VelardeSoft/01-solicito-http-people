6c8c23e1f69cc8cacc9ed1a973779e0c11db7a06
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2025-10-17 18:48:05 -0500

    Agregar archivos de proyecto.

---
+++ Solicito-http-people/Controllers/PeopleController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("All")] // All regresa todas las personas
+        public List<People> GetPeople() => Repository.People;
+
+    }
+
+    public class Repository  // Para simular database, lista de personas 
+    {
+        public static List<People> People = new List<People>
+        {
+            new People ()
+            {
+                Id = 1,
+                Name = "Mamita",
+                Birthdate = new DateTime(1999, 12, 2)
+            },
+            new People ()
+            {
+                Id = 2,
+                Name = "Teresita",
+                Birthdate = new DateTime(2000, 12, 2)
+            },
+            new People ()
+            {
+                Id = 3,
+                Name = "Messo",
+                Birthdate = new DateTime(2021, 12, 2)
+            },
+            new People ()
+            {
+                Id = 4,
+                Name = "Delegada",
+                Birthdate = new DateTime(2022, 12, 2)
+            }
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
